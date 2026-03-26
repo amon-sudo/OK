51cac6a4650bdb9b7f0f4e1c6d306b162ca16fe5
amon-sudo <amonogino@gmail.com>
2026-03-26 23:35:05 +0300

    ww

--- App/auth/Gaius.py
+++ App/auth/Gaius.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from Caeser import louis_bp
+from models.Caeser import louis_bp
 
 app = Flask(__name__)
 
@@ -7,4 +7,6 @@ app = Flask(__name__)
 app.register_blueprint(louis_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        
+     app.run(debug=True)
