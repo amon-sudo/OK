d897ff681399dfe52d73d922236e743d22230fff
amon-sudo <amonogino@gmail.com>
2026-03-28 23:33:53 +0300

    f

--- App/auth/Gaius.py
+++ App/auth/Gaius.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from auth.controllers.routes import amon_bp
-
+from auth.models import db, User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'amon-admiral-zheng-he'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
+db.init_app(app)
 
 app.register_blueprint(amon_bp)
 
--- App/auth/controllers/Caeser.py
+++ App/auth/controllers/Caeser.py
@@ -5,13 +5,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
 from wtforms.validators import DataRequired, Email, EqualTo
-from auth.models import db, User
 
-app =  Flask(__name__)
-app.config['SECRET_KEY'] = 'amon-admiral-zheng-he'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
-db.init_app(app)
+
+
 
 class Register(FlaskForm):
     fname = StringField('first name', validators=[DataRequired()])
